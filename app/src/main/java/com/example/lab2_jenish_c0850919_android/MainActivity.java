17c7d76dd45fe052127b2265aab637f25193acef
c0850919 <c0850919@mylambton.ca>
2022-06-13 17:43:03 -0400

    Add thread for handling jumping automatically after 5 seconds from lottie animation to another page.

--- app/src/main/java/com/example/lab2_jenish_c0850919_android/MainActivity.java
+++ app/src/main/java/com/example/lab2_jenish_c0850919_android/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lab2_jenish_c0850919_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        //For hiding actionbar...
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Thread td=new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    sleep(5000);
+                }
+
+                catch (Exception ex)
+                {
+                    ex.printStackTrace();
+                }
+
+                finally
+                {
+                    Intent intent = new Intent(MainActivity.this,homePage.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+        };
+        td.start();
     }
 }
